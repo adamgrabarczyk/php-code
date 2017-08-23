0836d70d7d4203ec0f2a19208bff1cde01bb340a
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-23 23:38:46 +0200

    use $ to add variables name and = to get value

---
+++ variables.php
@@ -0,0 +1,19 @@
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>title</title>
+    </head>
+    <body>
+
+<?php
+$name_var = 4;
+
+echo $name_var;
+?>
+    </body>
+</html>
+
+
+
+
