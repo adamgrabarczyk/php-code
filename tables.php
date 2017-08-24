03e5b6bd0694ba242d8c38babc9daafdd885daa2
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-24 22:21:25 +0200

    two most popular define table's

---
+++ tables.php
@@ -0,0 +1,37 @@
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>table's</title>
+        
+    </head>       
+    <body>
+        
+        
+<?php
+$table_name[0] = 10;
+$table_name[1] = 20;
+
+
+echo $table_name[0]."<br/>";
+echo $table_name[1]."<br/><br/>";
+
+$liczby[0] = 10;
+$liczby[1] = 20;
+$liczby[2] = 30;
+$liczby[3] = 40;
+
+echo $liczby[0]."<br/><br/>";
+
+$tab[0] = "cos";
+$tab[1] = 50;
+
+
+echo $tab[0]."<br/><br/>";
+echo $tab[1];
+
+?>
+    </body>
+</html>
+
+
