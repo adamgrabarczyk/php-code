a384386306bbee3489802faba1cdc360165b0a9b
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-08-25 19:25:47 +0200

    the simplest operators

---
+++ arithmeticOperators.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>arithmeticOperators</title>
+    </head>
+    <body>
+        <?php
+
+        $sm = 10;
+        $sm2 = 20;
+        
+        echo $sm + $sm2;
+        
+        echo '<br/>--------------<br/>';
+        echo $sm - $sm2;
+        echo '<br/>--------------<br/>';
+        echo $sm * $sm2;
+                echo '<br/>--------------<br/>';
+        echo $sm / $sm2;
+                echo '<br/>--------------<br/>';
+        echo 4 % 3;
+        echo '<br/>--------------<br/>';
+        echo "adam"." "."grabarczyk";
+        
+        
+        ?>
+    </body>
+</html>
