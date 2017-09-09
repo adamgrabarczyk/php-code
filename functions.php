10b0db83734d6978d22856779732ed5e9a9f2bae
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-09 18:57:10 +0200

    itroduction to php function

---
+++ functions.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Functions PHP</title>
+    </head>
+    <body>
+        <?php
+
+        $stringLen = strlen("test");
+        
+        echo $stringLen;
+        
+        ?>
+    </body>
+</html>
