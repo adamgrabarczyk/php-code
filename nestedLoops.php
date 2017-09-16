548c2d363a985c2f273f787ac55c2138b26d8da2
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-16 13:00:03 +0200

    use table tags

--- nestedLoops.php
+++ nestedLoops.php
@@ -10,15 +10,19 @@
         
         // pętle zagnieżdżone (petla w petli)
          
+        echo '<table>';
+        
         for($i = 1; $i <= 10; $i++){
             
+            echo '<tr>';
+            
             for ($j = 1; $j <= 10; $j++ )
-           echo ($i*$j)." ";
+           echo "<td>".($i*$j)."</td>";
             
             echo '<br />';
-            
+            echo '</tr>';
         }
-        
+        echo '</table>';
         
         ?>
     </body>
