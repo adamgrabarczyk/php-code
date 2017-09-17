4f192f8ea2665a8edafcfa29472a97b349f83e94
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-17 10:53:20 +0200

    itroduction to predefiniction varables

---
+++ predefinitoinVar/predefinitionVaraibles.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        
+       
+        
+        
+        <link rel="stylesheet" href="StyleSheet.css" type="text/css" />
+        <script type="text/javascript" src="js.js"></script>
+
+    </head>
+    <body>
+        <div id="container">
+            <div id="top">
+                BANER
+            </div>
+            
+            <div id="menu">
+                <div><a href="?page=java">Basketball</a></div>
+                <div><a href="?page=php">Football</a></div>
+                <div><a href="?page=mysql">Tenis</a></div>
+            </div>
+            <div id="content">     
+                <div id="text">
+        <?php
+        
+        if($_GET['akcja'] == "QRA");
+echo 'jestes na te jstronie';
+        
+        
+        echo '<br/>';
+        
+        echo '<br/>----------------------</br>';
+                echo 'lorem ipsumsssssssssssssssssssssssssssssssssssssssssssssssssmdlasldasdkaksda';
+        ?>
+               </div>
+           </div>
+
+        </div>
+    </body>
+</html>
