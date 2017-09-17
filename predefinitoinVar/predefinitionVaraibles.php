d653e142e7a2fceb363edcf1957f20ad615d44d2
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-17 12:04:59 +0200

    add few security line

--- predefinitoinVar/predefinitionVaraibles.php
+++ predefinitoinVar/predefinitionVaraibles.php
@@ -36,22 +36,36 @@
         if (isset($_GET['page']))          //isset sprawdza czy isnieje dany zakres
         {   
             
+            $allowd_pages = array("basketball","football","tenis"); // tablica nazw stron które dostaną zezwolenie na otwarcie przez nasz kod
+            
             $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);     // filtrowanie zmiennych      
             
+            
+           
             if (!empty($page))               // empty - zwraca prawda/falsz jezeli zmienna jest pusta/niepusta
-            {
+            
+                if(!in_array($page, $allowd_pages)) 
+               echo 'taka strona nie istnieje';
+           else {
+               
+           
+                {
                 
                     if (is_file($page.".html"))           // is_file sprawdza czy istnieje jakis plik
                         include($page.".html");           // include - dołącza plik
                     else
                         echo "Taka strona nie istnieje";
                 }
-            }
+        
+           }
+                
+                }else 
+                include ('start.html');
         
         echo '<br/>';
         
         echo '<br/>----------------------</br>';
-                echo 'lorem ipsumsssssssssssssssssssssssssssssssssssssssssssssssssmdlasldasdkaksda';
+                
         ?>
                </div>
            </div>
