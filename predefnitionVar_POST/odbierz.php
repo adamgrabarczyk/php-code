d16061e263d0dade13c3df89324473212bb0c2f5
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-19 19:27:54 +0200

    keep validate and print succes info

--- predefnitionVar_POST/odbierz.php
+++ predefnitionVar_POST/odbierz.php
@@ -1,6 +1,27 @@
 <?php
 
-echo $_POST['login'];
-echo $_POST['haslo'];;
+if (isset($_POST['login']) && isset($_POST['haslo']))
+{
+    
+    if(!empty($_POST['login']) && !empty($_POST['haslo']))
+    {
+        
+        $login = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
+        
+        $haslo = filter_var($_POST['haslo'], FILTER_SANITIZE_STRING);
+     
+        if($login == "Adam" && $haslo == "zabka") {
+            
+            echo 'jestes zalogowany';
+        }
+        
+    } else {
+    
+        echo 'nie podałes loginu lub hasła';
+    }
+} else {
+
+    echo 'nie podales hasla lub loginu';    
+}
 
 ?>
