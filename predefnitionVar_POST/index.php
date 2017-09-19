4f05b71a2b299f90333cb9e9e229811aaef130c4
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-19 19:07:32 +0200

    create login form

---
+++ predefnitionVar_POST/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+
+    <forn action="odbierz.php" method="post" enctype="multipart/form-data">
+        <div>
+            
+            <div>
+                login:<input type="text" name="login" maxlength="8" size="5">
+            </div>
+            
+            <div>password: <input type="password" name="haslo" maxlength="15" size="5"> </div>
+            
+            <div>
+                <input type="submit" value="Zaloguj się"/>
+                
+            </div>
+            
+            
+        </div>
+        
+        
+        
+    </forn>
+        
+        
+
+        <?php
+
+        ?>
+    </body>
+</html>
