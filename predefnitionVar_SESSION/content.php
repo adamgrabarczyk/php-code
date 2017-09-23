4d139125b2f7a19d6eef6d3d449da3b706dc6b72
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-09-23 09:30:00 +0200

    finish log panel with securyty things

---
+++ predefnitionVar_SESSION/content.php
@@ -0,0 +1,10 @@
+
+<?php
+        
+        echo "Gratulacje wlogowałeś się na konto: ".$_SESSION['login']."<br />";
+        echo "PANEL ADMINISTRACYJNY<br />";
+        echo "<a href='index.php'>Odśwież</a><br />";
+        echo "<a href='index.php?akcja=wyloguj'>Wyloguj się</a><br />";
+                        
+?>
+
--- predefnitionVar_SESSION/index.php
+++ predefnitionVar_SESSION/index.php
@@ -1,90 +1,95 @@
 <!DOCTYPE html>
 
-<?php
-  
-session_start(); // od tego czasu mozemy tworzyc zmienne sesyjne $_SESSION['nazwa'] = wartość.
 
-
-    ?>
-
+  <?php
+    session_start(); // od tego czasu mozemy tworzyc zmienne sesyjne $_SESSION['nazwa'] = wartość.
+    
+    if (!isset($_SESSION['initiate']))
+    {
+        session_regenerate_id();
+        $new_session_id = session_id();
+        session_write_close();
+        session_id($new_session_id);
+        session_start();        
+        $_SESSION['initiate'] = 1;
+    }
+?>
 
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+     
+        <title>Zmienna Predefiniowana $_SESSION - by Arkadiusz Włodarczyk - videokurs.pl</title>
+
     </head>
-    
-    
-    
-    
     <body>
 
+        <?php
+            if (isset($_GET['akcja']) && $_GET['akcja'] == "wyloguj")
+            {
+                $_SESSION['zalogowany'] = 0;
+                session_destroy();
+                echo "Zostałeś pomyślnie wylogowany<br />";
+            }
+            if ($_SESSION['zalogowany'] == 1 && (time() - $_SESSION['time'] > 10*60))
+            {
+                $_SESSION['zalogowany'] = 0;
+                session_destroy();
+                echo "Sesja zakończona, zbyt długa nieczynność. Prosimy o ponowne zalogowanie się<br />";               
+            }            
+            if ($_SESSION['zalogowany'] == 1 && ($_SESSION['info_o_komp'] != $_SERVER['HTTP_USER_AGENT']))
+            {
+                $_SESSION['zalogowany'] = 0;
+                session_destroy();
+                echo "Prosimy o ponowne zalogowanie się.";                                
+            }
+            if ((isset($_POST['login']) && isset($_POST['haslo'])) || $_SESSION['zalogowany'] == 1)
+            {
+                if ((!empty($_POST['login']) && !empty($_POST['haslo'])) || $_SESSION['zalogowany'] == 1)
+                {
+                    if ($_SESSION['zalogowany'] == 0)
+                    {
+                        $login = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
+                        $haslo = filter_var($_POST['haslo'], FILTER_SANITIZE_STRING);
+                    }
+                    
+                    if (($login == "Adam" && $haslo == "zabka") || $_SESSION['zalogowany'] == 1)
+                    {
+                        if ($_SESSION['zalogowany'] == 0)
+                            $_SESSION['login'] = $login;
+                        
+                        include ("content.php");
+                        $_SESSION['zalogowany'] = 1;
+                        $_SESSION['time'] = time();
+                        $_SESSION['info_o_komp'] = $_SERVER['HTTP_USER_AGENT'];
+                    }
+                    else
+                        echo "Podałeś niepoprawny login lub hasło. Spróbuj ponownie <a href='index.php'>tutaj</a>";
+                    
+                }   
+                else
+                    echo "Nie podałeś loginu lub hasła. Spróbuj ponownie <a href='index.php'>tutaj</a>";
+                 
+            }
+            
+           if ($_SESSION['zalogowany'] == 0)
+           {
+        ?>
         <form action="index.php" method="post" enctype="multipart/form-data">
-        <div>
-            
             <div>
-                login:<input type="text" name="login" maxlength="8" size="5">
+                <div>
+                    Login: <input type="text" name="login" maxlength="8" size="5" />
+                </div>
+                <div>
+                    Password: <input type="password" name="haslo" maxlength="15" size="5" />
+                </div> 
+                <div>
+                    <input type="submit" value="Zaloguj się" />
+                </div>
             </div>
-            
-            <div>password: <input type="password" name="haslo" maxlength="15" size="5"> </div>
-            
-            <div>
-                <input type="submit" value="Zaloguj się"/>
-                
-            </div>
-            
-            
-        </div>
-        
-        
-        
-    </forn>
-        
-        
-
-       <?php
-
-       
-       if(isset($_GET['akcja']) && $_GET['akcja'] == 'wyloguj') {
-           
-            $_SESSION['zalogowany'] = 0;
-           
-            echo 'zostałeś wylogowany <br/>';
-       }
-       
-if ((isset($_POST['login']) && isset($_POST['haslo'])) || $_SESSION['zalogowany'] == 1)
-{
-    
-    if((!empty($_POST['login']) && !empty($_POST['haslo'])) || $_SESSION['zalogowany'] == 1)
-    {
-        
-        $login = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
-        
-        $haslo = filter_var($_POST['haslo'], FILTER_SANITIZE_STRING);
-     
-        if(($login == "Adam" && $haslo == "zabka") || $_SESSION['zalogowany'] == 1) {
-            
-            echo 'jestes zalogowany: '.$login."<br/>";
-            echo 'panel administracyjny <br/>';
-            echo '<a href="index.php">odswiez</a><br/>';
-            echo '<a href="index.php?akcja=wyloguj">wyloguj</a><br/>';
-            
-            $_SESSION['zalogowany'] = 1 ;
-            
-        } else {
-        
-            echo 'podałeś niepoprawny login lub hasło';
-        }
-        
-    } else {
-    
-        echo 'nie podałes loginu lub hasła';
-    }
-} else {
-
-//    echo 'nie podales hasla lub loginu';    
-}
-
-?>
+        </form>    
+        <?php
+           }
+        ?>
     </body>
 </html>
