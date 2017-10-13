bda7be917493a3f54ee05a38cdb624599c939c87
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-13 21:02:20 +0200

    display date

--- ObjectProgramingIntroduction/ClassAndObject.php
+++ ObjectProgramingIntroduction/ClassAndObject.php
@@ -3,27 +3,15 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Send Email</title>
+        <title>Object and Class
+   </title>
     </head>
     <body>
         <?php
 
-            $do    = "emailadresata@przyklad.pl";
-            $temat = "Temat wiadomości";
-            $tresc = "<b>Testowy</b> e-mail do $do o treśći\n<br />".
-                     "Tekst w następnej lini\n".
-                     "Jeszcze jedna linia";
-            $tresc = wordwrap($tresc, 70);
-            $tresc = str_replace("\n.", "\n..", $tresc);
-            
-            $naglowki = "Content-type: text/html; charset=UTF8\r\n".
-                        "From: "."naszemail@domena.pl"."\r\n".
-                        "Reply-to: "."naszemail@domena.pl"."\r\n";
-            
-            
-            mail($do, $temat, $tresc, $naglowki);
-            
-            //text/html
+        
+        echo date("d M Y", time() );
+       
                    
         
         
