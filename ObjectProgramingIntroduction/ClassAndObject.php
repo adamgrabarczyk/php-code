3f4f9abae49f02093836a1558a0d762a5d366576
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-13 21:23:42 +0200

    import time.php using incule methods

--- ObjectProgramingIntroduction/ClassAndObject.php
+++ ObjectProgramingIntroduction/ClassAndObject.php
@@ -9,21 +9,8 @@
     <body>
         <?php
 
-        
-        date_default_timezone_set("Europe/Warsaw");
-        
-        
-        function getFutureTime ($days, $pattern = "H:i:s d M Y") {
-            
-            return date( $pattern, strtotime("+ $days days") );;;
-        }
-
-
-        function getCurrentTime($pattern = "H:i:s d M Y") {
-           
-            
-            return date( $pattern, time() );;
-        }
+        include ('time.php');
+       
 
 
         echo getCurrentTime()."<br/>";
--- ObjectProgramingIntroduction/time.php
+++ ObjectProgramingIntroduction/time.php
@@ -1,8 +1,19 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+ date_default_timezone_set("Europe/Warsaw");
+        
+        
+        function getFutureTime ($days, $pattern = "H:i:s d M Y") {
+            
+            return date( $pattern, strtotime("+ $days days") );;;
+        }
+
+
+        function getCurrentTime($pattern = "H:i:s d M Y") {
+           
+            
+            return date( $pattern, time() );;
+        }
+
+?>
