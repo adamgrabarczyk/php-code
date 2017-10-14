e779c8693d00756d82d3f811f2a3f08eb4581a03
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-14 23:37:54 +0200

    call parent construct

--- ClassHeirdom/Point2D.php
+++ ClassHeirdom/Point2D.php
@@ -8,8 +8,9 @@ class Point2D extends Point {
 //   public $x;   // trzeba określić modyfikator dostępu (public)
 //    protected $y; 
     
-    private $y;    // ta zmienna jest dostępna tylko wewnątrz tej klasy
+    protected $y;  
 
+    private $p;      // ta zmienna jest dostępna tylko wewnątrz tej klasy
 
     public function __construct($x = 0, $y = 0) {     // ustawiamy wartosc domyslna dla x
         
