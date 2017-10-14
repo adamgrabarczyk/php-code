0dae323d97e002986b5d43b11654466b2a6acff5
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-14 22:58:56 +0200

    create new pkt

--- ClassHeirdom/point.php
+++ ClassHeirdom/point.php
@@ -6,8 +6,8 @@ class Point {
     
    public $x = 50;   // trzeba określić modyfikator dostępu (public)
     
-   public function __construct($x) {
-       
+   public function __construct($x = 0) {     // ustawiamy wartosc domyslna dla x
+        
        
        $this->x = $x;
    }
