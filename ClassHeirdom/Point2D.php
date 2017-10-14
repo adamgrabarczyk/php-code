e808213977952d0b1d73bf1dd6547bdf21efa230
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-14 23:34:04 +0200

    add new Point3D, and show that can't call y becouse in 2D has protected

--- ClassHeirdom/Point2D.php
+++ ClassHeirdom/Point2D.php
@@ -6,10 +6,12 @@ class Point2D extends Point {
     
     
 //   public $x;   // trzeba określić modyfikator dostępu (public)
-    protected $y; 
+//    protected $y; 
+    
+    private $y;    // ta zmienna jest dostępna tylko wewnątrz tej klasy
 
 
-   public function __construct($x = 0, $y = 0) {     // ustawiamy wartosc domyslna dla x
+    public function __construct($x = 0, $y = 0) {     // ustawiamy wartosc domyslna dla x
         
        
        parent::__construct($x);
