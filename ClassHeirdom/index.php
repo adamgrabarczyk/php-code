32ec921c81dba50896424943656121bb637215ff
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-14 22:51:10 +0200

    remind some code from last lesons

--- ClassHeirdom/index.php
+++ ClassHeirdom/index.php
@@ -3,52 +3,20 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Object and Class
+        <title> Class heirdom
    </title>
     </head>
     <body>
         <?php
 
-        require_once('Class.time.php');          // wymaga tego pliku do wykonania dalszej częsci kodu (dołącza tylko raz)
-       
-
-
         
-       $a =  new Time();
-       
-       $b =  new Time("America/Atka");
-        
-        $c = new Time("Australia/Yancowinna");
-       
-       echo $a->setFutureTime(5)."<br/>";
-       echo $b->setFutureTime(10)."<br/>";
-       
-       
-       echo $a->futureTime."<br/>";
-       $b->setFutureTime(2);
-       echo $b->futureTime."<br/><br/>";
-       
-       echo $a."<br/><br/>";
-       
-       echo $b."<br/><br/>";
-       
-       echo $c."<br/><br/>";
-       
-       
-       $a->setFutureTime("4");
-       
-       echo $a->futureTime."<br/><br/>";
-       
-       echo $a->timeZone."<br/>";
-       echo $b->timeZone."<br/>";
-       echo $c->timeZone."<br/><br/>";
-//        echo getCurrentTime()."<br/>";
-//        echo getFutureTime(10)."<br/>";
-//       
-                   
+        require_once("point.php");
         
         
+       $p = new Point();
         
+       
+       echo $p->x."<br/>";
         
         ?>
     </body>
--- ClassHeirdom/point.php
+++ ClassHeirdom/point.php
@@ -1,61 +1,13 @@
 <?php
 
-
-
-class Time{
-
-
-// date_default_timezone_set("Europe/Warsaw");
-    const DEFAULT_TIME_ZONE = "Europe/Warsaw";        
-        
-    public $futureTime;
-        
-    public $currentTime;
-    
-    public $timeZone;
-    
-    function __construct($timeZone =self:: DEFAULT_TIME_ZONE ) {    // self:: oznacza odwołanie do siebie(danej klasy).  
-
-        $this->timeZone = $timeZone;                               // za pomocą this odwolujemy sie do wyzej zdefiniowanego time zione a bez this do tego w parametrze
-        
-date_default_timezone_set($this->timeZone);
-
-
-        $this->currentTime = $this->getCurrentTime();                     // konstruktor jest wywoływany jeden raz dla każdej instancji klasy
-    }
-    
-    
-    function __toString() {
-        return $this->currentTime;
-    }
-            
+class Point {
     
     
     
-            
-        function setFutureTime ($days, $pattern = "H:i:s d M Y") {
-            
-            $this->setTimeZone();
-            
-            $this->futureTime = date( $pattern, strtotime("+ $days days") );;
-            
-            return $this->futureTime;
-        }
-
-
-        function getCurrentTime($pattern = "H:i:s d M Y") {
-           
-            $this->setTimeZone();
-            
-            return date( $pattern, time() );;
-        } 
-        
-        function setTimeZone() {
-            if($this->timeZone != date_default_timezone_get())
-            date_default_timezone_set($this->timeZone);
-            
-            
-        }
-        
+   public $x = 5;   // trzeba określić modyfikator dostępu
+    
+    
 }
+
+
 ?>
