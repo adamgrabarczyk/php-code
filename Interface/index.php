af56962745e35d93271154f0c57c982e441ac800
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-15 19:52:55 +0200

    Interface file ready to work

---
+++ Interface/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Interface
+   </title>
+    </head>
+    <body>
+        <?php
+
+      
+        
+        
+       
+        ?>
+    </body>
+</html>
