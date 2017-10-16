ab02b79cac8f24cde740be2a279f6266c35dd6fd
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-16 18:05:45 +0200

    create interface premmision

---
+++ StaticFunction/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Static function
+   </title>
+    </head>
+    <body>
+        <?php
+
+        ?>
+    </body>
+</html>
