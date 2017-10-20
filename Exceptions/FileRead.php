bab347b9274fc17992dcb3455c826d5710c2f94f
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-20 17:06:34 +0200

    constrct wihich check that file egsist

--- Exceptions/FileRead.php
+++ Exceptions/FileRead.php
@@ -1,8 +1,18 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class FileRead {
+    
+    protected $filePointer;
+    
+    function __construct($fileName) {
+        
+        if(!($this->filePointer = @fopen($fileName, "r")))
+        {
+            echo 'nie ma takiego pliku';
+        }
+    }
+    
+    
+}
 
+?>
