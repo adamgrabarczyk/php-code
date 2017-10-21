44ecfa26c8aeda98388ffeb9b00ed941ddf2bf41
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-21 15:41:49 +0200

    use __Class to display name of class

--- FinalKeyWord/index.php
+++ FinalKeyWord/index.php
@@ -12,18 +12,18 @@
         class A {             // słówko final oznacza ze klasaa jest ostateczn i nie może byc rozszerzana przez klase inna
        
            final function mA() {
-            echo 'to jest funkcja z klasy a';
+            echo 'to jest funkcja z klasy '.__CLASS__;
             }
         
         }
         
         class B extends A {
             
-            final function mA() {
+            
             
             }
             
-        }
+        
         
         
         $b = new B();
