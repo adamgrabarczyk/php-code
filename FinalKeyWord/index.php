46f5ac6adcdbd0ce874ab8f75f5f0c23972701c0
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-21 15:33:31 +0200

    fianall key word

---
+++ FinalKeyWord/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Final Key Word</title>
+    </head>
+    <body>
+        <?php
+
+        
+       final class A {
+            
+        }
+        
+        class B extends A {
+            
+            
+        }
+        
+//        echo 'final key word';
+        
+        ?>
+    </body>
+</html>
