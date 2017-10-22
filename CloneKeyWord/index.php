2ecd6ff935518d453ac5124d0958ef2fd6cbbe91
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-22 10:41:26 +0200

    intro to cloning

---
+++ CloneKeyWord/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Clone Key Word</title>
+    </head>
+    <body>
+        <?php
+
+        
+        $a = 50;
+        
+        $b = $a;
+        
+        
+        echo $a.'<br/>';
+
+        echo $b."<br/>";        
+        
+        ?>
+    </body>
+</html>
