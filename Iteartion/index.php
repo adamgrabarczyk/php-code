a127457e7d17d96a1d18ae9325dae2c8cd949268
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-22 16:06:46 +0200

    iterate function

---
+++ Iteartion/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Iteration</title>
+    </head>
+    <body>
+        <?php
+
+        class A {
+            
+            public $a = "one";
+            
+            protected $b = "two";
+            
+            private $c = "three";
+            
+            public function iterate() {
+         
+                foreach ($this as $key => $value)
+        {
+            echo "key = $key <br/>";
+            
+            echo "value = $value <br/><br/>";
+        }
+                
+            }
+        
+        }
+        
+        
+        $a = new A();
+        
+//        foreach ($a as $key => $value)
+//        {
+//            echo "key = $key <br/>";
+//            
+//            echo "value = $value <br/><br/>";
+//        }
+        $a->iterate();
+        
+    
+        ?>
+    </body>
+</html>
