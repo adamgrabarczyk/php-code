08042730c9f3c211994205bde8b844736ebae954
adamgrabarczyk <adamgrabarczyk.gda@wp.pl>
2017-10-23 18:22:49 +0200

    introduction to mysql

---
+++ introMySQL/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>intro to MySQL</title>
+    </head>
+    <body>
+        <?php
+
+
+
+
+
+
+
+        ?>
+    </body>
+</html>
